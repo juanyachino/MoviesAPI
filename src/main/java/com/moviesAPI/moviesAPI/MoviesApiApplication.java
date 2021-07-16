52f26cc98571fcefaa473ecc58ad986b49013fcc
juanyachino <juanyachino@gmail.com>
2021-07-16 19:34:32 -0300

    removed /Hello  example route and unused imports

--- src/main/java/com/moviesAPI/moviesAPI/MoviesApiApplication.java
+++ src/main/java/com/moviesAPI/moviesAPI/MoviesApiApplication.java
@@ -4,23 +4,13 @@ package com.moviesAPI.moviesAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+;
 
 
 @SpringBootApplication
-@RestController
 public class MoviesApiApplication {
-
 	public static void main(String[] args) {
 		org.springframework.context.ConfigurableApplicationContext run = SpringApplication.run(MoviesApiApplication.class, args);
 	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
 }
 
