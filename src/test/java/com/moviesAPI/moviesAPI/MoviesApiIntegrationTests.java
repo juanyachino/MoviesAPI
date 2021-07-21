a78303aa363a347ac0777cd92dd584b233fe16c9
juanyachino <juanyachino@gmail.com>
2021-07-21 19:02:40 -0300

    fixed issue when creating accounts, now the database is dropped after every test run

--- src/test/java/com/moviesAPI/moviesAPI/MoviesApiIntegrationTests.java
+++ src/test/java/com/moviesAPI/moviesAPI/MoviesApiIntegrationTests.java
@@ -1,29 +1,22 @@
 package com.moviesAPI.moviesAPI;
 
-import com.moviesAPI.moviesAPI.repositories.UserRepository;
 import com.moviesAPI.moviesAPI.utils.MultiPartResource;
-import io.jsonwebtoken.Jwt;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.TestInstance;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.JsonPathAssertions;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.reactive.server.WebTestClientConfigurer;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -33,25 +26,24 @@ import org.springframework.web.reactive.function.BodyInserters;
 
 
 import java.io.IOException;
-import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.*;
-import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(properties="spring.jpa.hibernate.ddl-auto=create-drop") // clears the database after every test run
 public class MoviesApiIntegrationTests {
 
+    private static final String TESTUSERNAME = "test1";
+    private static final String TESTPASSWORD = "test123";
+    private static final String TESTEMAIL = "test@email.com";
     @Autowired
     private WebTestClient webTestClient;
 
-    //@MockBean
-    //private UserRepository userRepository; //can't get a token if using this.
 
     MultipartFile image = new MockMultipartFile("image",new byte[12]);
 
@@ -59,13 +51,13 @@ public class MoviesApiIntegrationTests {
     String token = "";
 
 
-    @Before
-    public void accountCreation() throws IOException {
+    @BeforeAll
+    public void accountCreation()  {
 
         LinkedMultiValueMap map = new LinkedMultiValueMap();
-        map.add("username", "juan123");
-        map.add("password", "juan123");
-        map.add("email", "juan123@email.com");
+        map.add("username", TESTUSERNAME);
+        map.add("password", TESTPASSWORD);
+        map.add("email", TESTEMAIL);
         this.webTestClient
                 .post()
                 .uri("/auth/register")
@@ -76,9 +68,9 @@ public class MoviesApiIntegrationTests {
                 .isOk();
 
 
-        /*map = new LinkedMultiValueMap();
-        map.add("username", "juan123");
-        map.add("password", "juan123");
+        map = new LinkedMultiValueMap();
+        map.add("username", TESTUSERNAME);
+        map.add("password", TESTPASSWORD);
 
          this.token = this.webTestClient
                 .post()
@@ -90,8 +82,8 @@ public class MoviesApiIntegrationTests {
                  .returnResult(String.class)
                  .getResponseBody()
                  .blockFirst();
- */
     }
+
     @Test
     public void createAccountWithInvalidData(){
         LinkedMultiValueMap map = new LinkedMultiValueMap();
@@ -139,7 +131,7 @@ public class MoviesApiIntegrationTests {
                 .equalsIgnoreCase("Incorrect username and/or password. try again");
     }
     @Test
-    public void accessProtectedResourceWithoutAuth() throws IOException {
+    public void accessingProtectedResourceWithoutAuth() throws IOException {
         ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
         LinkedMultiValueMap map = new LinkedMultiValueMap();
         map.add("name", "Carol Danvers");
@@ -160,21 +152,40 @@ public class MoviesApiIntegrationTests {
 
     }
     @Test
-    public void createCharacterWithInvalidAge() throws IOException {
-        /*LinkedMultiValueMap map = new LinkedMultiValueMap();
-        map.add("username", "juan123");
-        map.add("password", "juan123");
-
-        String AuthToken = this.webTestClient
-                .post()
-                .uri("/auth/login").accept(MediaType.TEXT_PLAIN)
-                .body(BodyInserters.fromMultipartData(map))
-                .header(ACCEPT,APPLICATION_JSON_VALUE)
+    public void AccessingProtectedResourceWithTokenWorks() {
+        this.webTestClient
+                .get()
+                .uri("/characters")
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
                 .exchange()
-                .expectStatus().isOk()
-                .returnResult(String.class)
-                .getResponseBody()
-                .blockFirst(); */
+                .expectStatus()
+                .isOk();
+    }
+    @Test
+    public void createCharacterWithMissingFields() throws IOException {
+        ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
+        LinkedMultiValueMap map = new LinkedMultiValueMap();
+        map.add("name", "Carol Danvers");
+        map.add("age", 35);
+        map.add("multipartImage", resource);
+
+
+        this.webTestClient
+                .post()
+                .uri("/characters/add")
+                .body(BodyInserters.fromMultipartData(map))
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
+                .exchange()
+                .expectStatus()
+                .isBadRequest();
+
+    }
+    @Test
+    public void createCharacterWithInvalidAge() throws IOException {
+
         ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
         LinkedMultiValueMap map = new LinkedMultiValueMap();
         map.add("name", "Carol Danvers");
@@ -189,7 +200,7 @@ public class MoviesApiIntegrationTests {
                 .uri("/characters/add")
                 .body(BodyInserters.fromMultipartData(map))
                 .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
-                .headers(httpHeaders -> httpHeaders.setBearerAuth(loginAndGetToken("juan123","juan123")))
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
 
                 .exchange()
                 .expectStatus()
@@ -197,30 +208,26 @@ public class MoviesApiIntegrationTests {
 
     }
     @Test
-    public void AccessingProtectedResourceWithTokenWorks() {
+    public void createCharacterWithInvalidWeight() throws IOException {
+        ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
+        LinkedMultiValueMap map = new LinkedMultiValueMap();
+        map.add("name", "Carol Danvers");
+        map.add("age", 25);
+        map.add("story", "Also known as CaptainMarvel");
+        map.add("weight", -65);
+        map.add("multipartImage", resource);
+
+
         this.webTestClient
-                .get()
-                .uri("/characters")
+                .post()
+                .uri("/characters/add")
+                .body(BodyInserters.fromMultipartData(map))
                 .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
-                .headers(httpHeaders -> httpHeaders.setBearerAuth(loginAndGetToken("juan123","juan123")))
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
                 .exchange()
                 .expectStatus()
-                .isOk();
-    }
-    private String loginAndGetToken(String username, String password){
-        LinkedMultiValueMap map = new LinkedMultiValueMap();
-        map.add("username", username);
-        map.add("password", password);
+                .isBadRequest();
 
-        return this.webTestClient
-                .post()
-                .uri("/auth/login").accept(MediaType.TEXT_PLAIN)
-                .body(BodyInserters.fromMultipartData(map))
-                .header(ACCEPT,APPLICATION_JSON_VALUE)
-                .exchange()
-                .expectStatus().isOk()
-                .returnResult(String.class)
-                .getResponseBody()
-                .blockFirst();
     }
 }
