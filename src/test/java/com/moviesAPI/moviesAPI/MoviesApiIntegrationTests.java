61e0379dd4374b7badd4e0c822b4e431bd24d868
juanyachino <juanyachino@gmail.com>
2021-07-22 19:05:44 -0300

    added new tests

--- src/test/java/com/moviesAPI/moviesAPI/MoviesApiIntegrationTests.java
+++ src/test/java/com/moviesAPI/moviesAPI/MoviesApiIntegrationTests.java
@@ -1,16 +1,24 @@
 package com.moviesAPI.moviesAPI;
 
+import com.moviesAPI.moviesAPI.entities.Character;
+import com.moviesAPI.moviesAPI.repositories.CharacterRepository;
+import com.moviesAPI.moviesAPI.repositories.GenreRepository;
+import com.moviesAPI.moviesAPI.repositories.MovieRepository;
 import com.moviesAPI.moviesAPI.utils.MultiPartResource;
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ByteArrayResource;
 
 import org.springframework.http.MediaType;
@@ -26,6 +34,8 @@ import org.springframework.web.reactive.function.BodyInserters;
 
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Optional;
 
 import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -44,16 +54,25 @@ public class MoviesApiIntegrationTests {
     @Autowired
     private WebTestClient webTestClient;
 
+    @Autowired
+    private CharacterRepository characterRepository;
+    @Autowired
+    private MovieRepository movieRepository;
+    @Autowired
+    private GenreRepository genreRepository;
 
     MultipartFile image = new MockMultipartFile("image",new byte[12]);
 
 
     String token = "";
+    private Long characterId;
+    private Long movieId;
+    private Long genreId;
 
 
     @BeforeAll
-    public void accountCreation()  {
-
+    public void setup() throws IOException {
+        //User account creation
         LinkedMultiValueMap map = new LinkedMultiValueMap();
         map.add("username", TESTUSERNAME);
         map.add("password", TESTPASSWORD);
@@ -67,8 +86,8 @@ public class MoviesApiIntegrationTests {
                 .expectStatus()
                 .isOk();
 
-
-        map = new LinkedMultiValueMap();
+        //login to get the jwt
+        map.clear();
         map.add("username", TESTUSERNAME);
         map.add("password", TESTPASSWORD);
 
@@ -82,6 +101,33 @@ public class MoviesApiIntegrationTests {
                  .returnResult(String.class)
                  .getResponseBody()
                  .blockFirst();
+
+         //create a test  character
+        ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
+        map.clear();
+        map.add("name", "Carol Danvers");
+        map.add("age", 23);
+        map.add("story", "Also known as CaptainMarvel");
+        map.add("weight", 65);
+        map.add("multipartImage", resource);
+
+
+        this.webTestClient
+                .post()
+                .uri("/characters/add")
+                .body(BodyInserters.fromMultipartData(map))
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .isEqualTo("Character created successfully!");
+        characterId = obtainAnyValidCharacterId();
+        //create a  test movie
+
+        //create a test genre
     }
 
     @Test
@@ -99,7 +145,7 @@ public class MoviesApiIntegrationTests {
                 .expectStatus()
                 .isBadRequest();
 
-        map = new LinkedMultiValueMap();
+        map.clear();
         map.add("username", "Juan1");
         map.add("password", "j");
         map.add("email", "juan1@email.com");
@@ -118,17 +164,17 @@ public class MoviesApiIntegrationTests {
         map.add("username", "asdasda");
         map.add("password", "faasdfafaan");
         map.add("email", "asdasds@email.com");
+        Assertions.assertThat(map.containsKey("email"));
         this.webTestClient
                 .post()
                 .uri("/auth/login")
                 .body(BodyInserters.fromMultipartData(map))
                 .header(ACCEPT,APPLICATION_JSON_VALUE)
-                .exchange()
-                .expectStatus()
-                .is2xxSuccessful()
-                .expectBody()
-                .toString()
-                .equalsIgnoreCase("Incorrect username and/or password. try again");
+                .exchange().expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .isEqualTo("Incorrect username and/or password. try again");
+
     }
     @Test
     public void accessingProtectedResourceWithoutAuth() throws IOException {
@@ -184,8 +230,8 @@ public class MoviesApiIntegrationTests {
 
     }
     @Test
-    public void createCharacterWithInvalidAge() throws IOException {
-
+    public void createCharacterWithInvalidData() throws IOException {
+        // with invalid age
         ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
         LinkedMultiValueMap map = new LinkedMultiValueMap();
         map.add("name", "Carol Danvers");
@@ -204,18 +250,13 @@ public class MoviesApiIntegrationTests {
 
                 .exchange()
                 .expectStatus()
-                .isBadRequest();
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Character's age is invalid!");
+        // with invalid weight
+        map.set("age", 25);
+        map.set("weight", -65);
 
-    }
-    @Test
-    public void createCharacterWithInvalidWeight() throws IOException {
-        ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
-        LinkedMultiValueMap map = new LinkedMultiValueMap();
-        map.add("name", "Carol Danvers");
-        map.add("age", 25);
-        map.add("story", "Also known as CaptainMarvel");
-        map.add("weight", -65);
-        map.add("multipartImage", resource);
 
 
         this.webTestClient
@@ -227,7 +268,151 @@ public class MoviesApiIntegrationTests {
 
                 .exchange()
                 .expectStatus()
-                .isBadRequest();
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Character's weight is invalid!");
+    }
+    @Test
+    public void editACharacterWithoutAuth() throws IOException {
+        ByteArrayResource resource = new MultiPartResource(image.getBytes(), "image.jpg");
 
+        LinkedMultiValueMap map = new LinkedMultiValueMap();
+        map.add("id",1);
+        map.add("name", "Carol Danvers");
+
+        this.webTestClient
+                .post()
+                .uri("/characters/edit")
+                .body(BodyInserters.fromMultipartData(map))
+                .header(ACCEPT,APPLICATION_JSON_VALUE)
+                .exchange()
+                .expectStatus()
+                .isForbidden();
+    }
+    @Test
+    public void editACharacterWithInvalidData() throws IOException {
+        //with invalid age
+        LinkedMultiValueMap map = new LinkedMultiValueMap();
+        map.add("id",obtainAnyValidCharacterId());
+        map.add("age", -5);
+
+        this.webTestClient
+                .post()
+                .uri("/characters/edit")
+                .body(BodyInserters.fromMultipartData(map))
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Character's age is invalid!");
+        //With invalid weight
+        map.set("age", 25);
+        map.add("weight", -65);
+
+
+        this.webTestClient
+                .post()
+                .uri("/characters/edit")
+                .body(BodyInserters.fromMultipartData(map))
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("Character's weight is invalid!");
+    }
+    @Test
+    public void editCharacterWithMissingRequiredParameterId(){
+        LinkedMultiValueMap map = new LinkedMultiValueMap();
+        map.add("age", 10);
+
+        this.webTestClient
+                .post()
+                .uri("/characters/edit")
+                .body(BodyInserters.fromMultipartData(map))
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
+                .exchange()
+                .expectStatus()
+                .isBadRequest();
+    }
+    @Test
+    public void editCharacterWithAuthWorks(){
+        Long characterId = obtainAnyValidCharacterId();
+        String newName = "new edited name";
+        Integer newAge =  (int) ((Math.random() * (100 - 1)) + 1);
+        Integer newWeight = (int) ((Math.random() * (100 - 1)) + 1);
+        String newStory = "new edited story";
+        LinkedMultiValueMap map = new LinkedMultiValueMap();
+        map.add("id", characterId);
+        map.add("name", newName);
+        map.add("age", newAge);
+        map.add("weight", newWeight);
+        map.add("story", newStory);
+
+        this.webTestClient
+                .post()
+                .uri("/characters/edit")
+                .body(BodyInserters.fromMultipartData(map))
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        this.webTestClient
+                .get()
+                .uri("/characters/detail?id="+characterId)
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .jsonPath("name").isEqualTo(newName)
+                .jsonPath("age").isEqualTo(newAge)
+                .jsonPath("story").isEqualTo(newStory)
+                .jsonPath("weight").isEqualTo(newWeight);
+
+    }
+    @Test
+    public void deleteCharacterWithoutAuth(){
+        this.webTestClient
+                .delete()
+                .uri("/characters/delete?id="+1)
+                .header(ACCEPT,APPLICATION_JSON_VALUE)
+                .exchange()
+                .expectStatus()
+                .isForbidden();
+    }
+    @Test
+    public void deleteCharacterWithAuthWorks(){
+        this.webTestClient
+                .delete()
+                .uri("/characters/delete?id="+obtainAnyValidCharacterId())
+                .header(AUTHORIZATION,ACCEPT,APPLICATION_JSON_VALUE)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .isEqualTo("deleted!");
+    }
+    private Long obtainAnyValidCharacterId(){
+        Iterable<Character> character = characterRepository.findAll();
+        Iterator<Character> it = character.iterator();
+        long id = 1;
+        while (it.hasNext()){
+            id = it.next().getId();
+        }
+        return id;
     }
 }
