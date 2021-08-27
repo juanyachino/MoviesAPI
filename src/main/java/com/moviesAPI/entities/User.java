42fb342b83940adcada83cc411c83e7d09ebb47f
juanyachino <juanyachino@gmail.com>
2021-08-26 22:15:54 -0300

    added validations for username and password

--- src/main/java/com/moviesAPI/entities/User.java
+++ src/main/java/com/moviesAPI/entities/User.java
@@ -2,6 +2,7 @@ package com.moviesAPI.entities;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 
 @Entity // This tells Hibernate to make a table out of this class
@@ -13,9 +14,13 @@ public class User  {
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @Size(min = 5,message = "username must be at least 5 characters long")
     private String username;
+
     @Column(nullable = false)
+    @Size(min = 5,message = "password must be at least 5 characters long")
     private String password;
+
     @Column(nullable = false, unique = true)
     private String email;
     @Transient //doesn't persist the token
